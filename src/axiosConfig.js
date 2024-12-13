17a76d1576524fc133fea260b683ab47b6e257a6
Manish Pingale <manishpingale2005@gmail.com>
2024-12-13 11:01:48 +0530

    accessing deployed API

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+axios.defaults.baseURL = "https://socializer-backend-rh1z.onrender.com/api/";
 
 const username = localStorage.getItem("username");
 const password = localStorage.getItem("password");
